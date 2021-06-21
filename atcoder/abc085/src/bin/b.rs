99d61295bf7c0b39823b71b810873b9c13f2d9d8
curekoshimizu <shugo@xxxmanabe.sakura.ne.jp>
2021-06-21 23:05:35 +0900

    implement ABC 085 B

--- atcoder/abc085/src/bin/b.rs
+++ atcoder/abc085/src/bin/b.rs
@@ -1,5 +1,7 @@
 use argio::argio;
+use std::collections::HashSet;
 
 #[argio]
-fn main(n: u32, _a: [u32; n]) {
+fn main(n: u32, a: [u32; n]) -> u32 {
+    a.iter().collect::<HashSet<_>>().len() as u32
 }
