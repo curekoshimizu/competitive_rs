a2a07e2777c0990bac15dd9a3c28a37bc26ff652
curekoshimizu <shugo@xxxmanabe.sakura.ne.jp>
2021-06-21 22:44:55 +0900

    implement ABC 111 A, B

---
+++ atcoder/abc111/src/bin/b.rs
@@ -0,0 +1,16 @@
+use argio::argio;
+
+#[argio]
+fn main(n: u32) -> u32 {
+    (0..10)
+        .flat_map(|i| {
+            let num = 100 * i + 10 * i + i;
+            if n <= num {
+                return Some(num);
+            }
+
+            return None;
+        })
+        .next()
+        .unwrap()
+}
