84eb1cc0b8b9cf02f2bfabddda28eb158c31c18a
curekoshimizu <shugo@xxxmanabe.sakura.ne.jp>
2021-06-23 22:16:19 +0900

    ABC086 C

--- atcoder/abc086/src/bin/c.rs
+++ atcoder/abc086/src/bin/c.rs
@@ -1,5 +1,23 @@
 use argio::argio;
 
 #[argio]
-fn main(n: u32, _a: [u32; n]) {
+fn main(n: u32, a: [(i32, i32, i32); n]) -> &str {
+    let (mut t, mut x, mut y) = (0, 0, 0);
+
+    for (t1, x1, y1) in a.into_iter() {
+        let dt = t1 - t;
+        let dx = (x1 - x).abs();
+        let dy = (y1 - y).abs();
+
+        let ans = dt - dx - dy;
+        if !(ans >= 0 && ans % 2 == 0) {
+            return "No";
+        }
+
+        t = t1;
+        x = x1;
+        y = y1;
+    }
+
+    return "Yes";
 }
