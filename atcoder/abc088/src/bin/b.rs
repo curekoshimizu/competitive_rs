0993249de95b7c7ce12688b9d0f22373cf22600a
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-06-25 23:00:46 +0900

    ABC088 A, B

---
+++ atcoder/abc088/src/bin/b.rs
@@ -0,0 +1,16 @@
+use argio::argio;
+
+#[argio]
+fn main(n: u32, mut a: [u32; n]) -> u32 {
+    a.sort_by_key(|&x| std::cmp::Reverse(x));
+
+    let alice: u32 = a.iter().step_by(2).sum();
+
+    let mut b = a.iter();
+
+    b.next();
+
+    let bob: u32 = b.step_by(2).sum();
+
+    alice - bob
+}
