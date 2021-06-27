edffaa2f84744c10ded71c7475c49a934c259af6
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-06-27 19:44:10 +0900

    preview page

---
+++ memo_server/src/apps/ContestRow.tsx
@@ -0,0 +1,41 @@
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+
+import PreviewProblemButton from './PreviewProblemButton';
+
+const range = (n: number): number[] => [...Array(n).keys()];
+
+interface ContestRowProp {
+  contest: string;
+  numProblems: number;
+}
+
+const ContestRow: React.FC<ContestRowProp> = ({ contest, numProblems }) => {
+  return (
+    <Box display="flex" justifyContent="center" m={1} p={1}>
+      <Box width={300}>
+        <Typography component="h3" variant="h5">
+          {contest.replace('abc', 'abc ').toUpperCase()}
+        </Typography>
+      </Box>
+      <Box display="flex" width="100%">
+        {range(numProblems).map((idx) => {
+          const code: number = 'a'.charCodeAt(0);
+          const problem = String.fromCharCode(code + idx);
+
+          return (
+            <Box key={idx} width={200}>
+              <PreviewProblemButton
+                contest={contest}
+                numProblems={numProblems}
+                problem={problem}
+              />
+            </Box>
+          );
+        })}
+      </Box>
+    </Box>
+  );
+};
+
+export default ContestRow;
