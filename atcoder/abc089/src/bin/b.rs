784f503fda6b046bd692f3d72c947d14d72d0c44
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-06-28 21:47:46 +0900

    ABC089 B

--- atcoder/abc089/src/bin/b.rs
+++ atcoder/abc089/src/bin/b.rs
@@ -1,5 +1,16 @@
+// keywords : set
+
 use argio::argio;
 
+use std::collections::HashSet;
+
 #[argio]
-fn main(n: u32, _a: [u32; n]) {
+fn main(n: u32, a: [char; n]) -> &str {
+    let set = a.iter().collect::<HashSet<_>>();
+
+    match set.len() {
+        3 => "Three",
+        4 => "Four",
+        _ => panic!("!?"),
+    }
 }
