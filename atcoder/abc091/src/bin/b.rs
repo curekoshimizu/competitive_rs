60d63f1016fb7d5ca52513355d3fa4a7d7b5d69e
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-06-28 23:58:11 +0900

    ABC 091 B

--- atcoder/abc091/src/bin/b.rs
+++ atcoder/abc091/src/bin/b.rs
@@ -1,8 +1,22 @@
-// keywords : 
+// keywords : hash, entry, unwrap_or
 
 use argio::argio;
+use std::collections::HashMap;
 
 #[argio]
-fn main(n: u32, _a: [u32; n]) {
-    todo!();
+fn main(n: u32, a: [String; n], m: u32, b: [String; m]) -> u32 {
+    let mut red_map: HashMap<&String, u32> = HashMap::new();
+
+    for x in a.iter() {
+        let entry = red_map.entry(x).or_default(); // you can use .or_insert(0) as well
+        *entry += 1;
+    }
+    for x in b.iter() {
+        let entry = red_map.entry(x).or_default();
+        if *entry > 0 {
+            *entry -= 1;
+        }
+    }
+
+    *red_map.values().max().unwrap_or(&0)
 }
