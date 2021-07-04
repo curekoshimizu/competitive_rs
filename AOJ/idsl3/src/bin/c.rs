4772d82f895d83cb00d186083da50c88287f8db9
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-07-05 08:00:31 +0900

    AOJ IDSL1_3 C

--- AOJ/idsl3/src/bin/c.rs
+++ AOJ/idsl3/src/bin/c.rs
@@ -1,18 +1,51 @@
 // keywords :
 
-use argio::argio;
+use input_lib::Scanner;
 
-#[argio]
-fn main(n: u32, _a: [u32; n]) {
-    todo!();
+use std::collections::VecDeque;
+
+fn main() {
+    let mut sc = Scanner::new(std::io::stdin().lock());
+    let n = sc.next::<u64>().unwrap();
+
+    let mut list = VecDeque::new();
+    for _ in 0..n {
+        let command = sc.next::<String>().unwrap();
+        match &command as &str {
+            "insert" => {
+                let key = sc.next::<u64>().unwrap();
+                list.push_front(key);
+            }
+            "delete" => {
+                let key = sc.next::<u64>().unwrap();
+                match list.iter().position(|&x| x == key) {
+                    Some(at) => {
+                        if at == 0 {
+                            list.pop_front();
+                        } else if at == list.len() {
+                            list.pop_back();
+                        } else {
+                            list.remove(at);
+                        }
+                    }
+                    None => {}
+                };
+            }
+            "deleteFirst" => {
+                list.pop_front();
+            }
+            "deleteLast" => {
+                list.pop_back();
+            }
+            _ => panic!("!!!!!"),
+        };
+    }
+
+    println!(
+        "{}",
+        list.iter()
+            .map(|&s| s.to_string())
+            .collect::<Vec<_>>()
+            .join(" ")
+    );
 }
-
-// use input_lib::*;
-//
-// fn main() {
-//     input! {
-//         n: u32,
-//     }
-//
-//     println!("{}", n);
-// }
