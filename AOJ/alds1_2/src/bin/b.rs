fe4b92dc2dc5eadc7a39d809fe458d6b83f69e1f
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-07-06 10:23:52 +0900

    AOJ ALDS1_2 B

--- AOJ/alds1_2/src/bin/b.rs
+++ AOJ/alds1_2/src/bin/b.rs
@@ -1,18 +1,45 @@
 // keywords :
 
-use argio::argio;
+// 1 selectionSort(A, N) // N個の要素を含む0-オリジンの配列A
+// 2   for i が 0 から N-1 まで
+// 3     minj = i
+// 4     for j が i から N-1 まで
+// 5       if A[j] < A[minj]
+// 6         minj = j
+// 7     A[i] と A[minj] を交換
 
-#[argio]
-fn main(n: u32, _a: [u32; n]) {
-    todo!();
+use io_lib::*;
+
+fn main() {
+    input! {
+        n: usize,
+        a: [u32; n],
+    }
+
+    let mut count = 0;
+    let mut a = a;
+
+    for i in 0usize..n {
+        let mut min_j = i;
+        for j in i..n {
+            if a[j] < a[min_j] {
+                min_j = j;
+            }
+        }
+        if i != min_j {
+            count += 1;
+            let tmp = a[i];
+            a[i] = a[min_j];
+            a[min_j] = tmp;
+        }
+    }
+
+    println!(
+        "{}",
+        a.iter()
+            .map(|&i| i.to_string())
+            .collect::<Vec<_>>()
+            .join(" ")
+    );
+    println!("{}", count);
 }
-
-// use io_lib::*;
-//
-// fn main() {
-//     input! {
-//         n: u32,
-//     }
-//
-//     println!("{}", n);
-// }
