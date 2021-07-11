359add31c945d334ca898f5cd5245d2d3d6c5487
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-07-11 21:19:31 +0900

    AOJ ALDS1_4 C

--- AOJ/alds1_4/src/bin/c.rs
+++ AOJ/alds1_4/src/bin/c.rs
@@ -1,18 +1,45 @@
 // keywords :
 
-use argio::argio;
+use io_lib::*;
+use std::collections::HashSet;
 
-#[argio]
-fn main(n: u32, _a: [u32; n]) {
-    todo!();
+fn main() {
+    input! {
+        n: u32,
+        operations: [ (String, String); n],
+    }
+
+    let mut set: HashSet<(u64, usize)> = HashSet::new();
+
+    for op in operations {
+        match &op.0[..] {
+            "insert" => {
+                set.insert(hash(&op.1));
+            }
+            "find" => {
+                if set.contains(&hash(&op.1)) {
+                    println!("yes")
+                } else {
+                    println!("no")
+                }
+            }
+            _ => panic!("!!!!"),
+        }
+    }
 }
 
-// use io_lib::*;
-//
-// fn main() {
-//     input! {
-//         n: u32,
-//     }
-//
-//     println!("{}", n);
-// }
+fn hash(x: &String) -> (u64, usize) {
+    let mut result = 0u64;
+    for (i, c) in x.chars().enumerate() {
+        let y: u64 = match c {
+            'A' => 0x0,
+            'C' => 0x1,
+            'G' => 0x2,
+            'T' => 0x3,
+            _ => panic!("!!!"),
+        } << (i * 2);
+        result |= y;
+    }
+
+    (result, x.len())
+}
