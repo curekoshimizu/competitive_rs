dc0e8d65ea96e846ee1e4c86b9a595f946a79f96
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-07-18 22:26:18 +0900

    implement intersection point is on both segment lines

--- AOJ/cgl2/src/bin/b.rs
+++ AOJ/cgl2/src/bin/b.rs
@@ -1,16 +1,30 @@
 // keywords :
 
-use argio::argio;
+use geometory2d::line2d::Line2d;
+use geometory2d::point2d::Point2d;
 
-#[argio]
-fn main(n: u64, _a: [u64; n]) {
-    todo!();
+use io_lib::*;
+
+fn main() {
+    input! {
+        q: usize,
+        p: [f64; 8* q],
+    }
+
+    for i in 0..q {
+        let base = 8 * i;
+        let p0 = Point2d::new(p[0 + base], p[1 + base]);
+        let p1 = Point2d::new(p[2 + base], p[3 + base]);
+        let p2 = Point2d::new(p[4 + base], p[5 + base]);
+        let p3 = Point2d::new(p[6 + base], p[7 + base]);
+
+        let line1 = Line2d::new(&p0, &p1);
+        let line2 = Line2d::new(&p2, &p3);
+
+        if line1.is_intersection_point_on_both_segment(&line2) {
+            println!("1");
+        } else {
+            println!("0");
+        }
+    }
 }
-
-// use io_lib::*;
-//
-// fn main() {
-//     input! {
-//         n: u64,
-//     }
-// }
--- competition_lib/geometory2d/src/line2d.rs
+++ competition_lib/geometory2d/src/line2d.rs
@@ -1,6 +1,8 @@
 use super::point2d::Point2d;
 use super::vec2d::Vec2d;
 
+const EPS: f64 = 1.0e-5;
+
 #[derive(PartialEq, Debug, Clone, Copy)]
 pub struct Line2d<'a> {
     start: &'a Point2d,
@@ -31,6 +33,50 @@ impl<'a> Line2d<'a> {
         let mid = self.project(p);
         let v = (mid - p) * 2.0;
         p + v
+    }
+    pub fn is_on_line(&self, p: &Point2d) -> bool {
+        (p - self.start).det(&(self.end - p)).abs() < EPS
+    }
+    /// check whether p is on line and between start point and end point
+    /// including point start and ponit end.
+    pub fn is_on_segment(&self, p: &Point2d) -> bool {
+        if !self.is_on_line(p) {
+            false
+        } else {
+            (p - self.start).dot(&(self.end - p)) > -EPS
+        }
+    }
+    pub fn intersection_point(&self, line: &Line2d) -> Option<Point2d> {
+        let vec1 = self.vector();
+        let vec2 = line.vector();
+
+        let b = vec1.det(&(self.start - line.start));
+        let a = vec1.det(&vec2);
+
+        let t = b / a;
+
+        if !t.is_normal() {
+            if self.is_on_segment(&line.start) {
+                Some(*line.start)
+            } else if self.is_on_segment(&line.end) {
+                Some(*line.end)
+            } else if line.is_on_segment(&self.start) {
+                Some(*self.start)
+            } else if line.is_on_segment(&self.end) {
+                Some(*self.end)
+            } else {
+                None
+            }
+        } else {
+            Some(line.start + t * vec2)
+        }
+    }
+    pub fn is_intersection_point_on_both_segment(&self, line: &Line2d) -> bool {
+        if let Some(p) = self.intersection_point(line) {
+            self.is_on_segment(&p) && line.is_on_segment(&p)
+        } else {
+            false
+        }
     }
 }
 
@@ -139,7 +185,126 @@ mod tests {
                 .is_almost_zero()
         );
     }
+    #[test]
+    fn on_line() {
+        let a = Point2d::new(0.0, 0.0);
+        let b = Point2d::new(1.0, 0.0);
+        let line = Line2d::new(&a, &b);
+        assert!(line.is_on_line(&Point2d::new(2.0, 0.0)));
+        assert!(line.is_on_line(&Point2d::new(1.0, 0.0)));
+        assert!(line.is_on_line(&Point2d::new(0.5, 0.0)));
+        assert!(line.is_on_line(&Point2d::new(0.0, 0.0)));
+        assert!(line.is_on_line(&Point2d::new(-1.0, 0.0)));
+        assert!(!line.is_on_line(&Point2d::new(1.0, 0.1)));
+        assert!(!line.is_on_line(&Point2d::new(0.5, 0.1)));
+        assert!(!line.is_on_line(&Point2d::new(0.0, 0.1)));
+        assert!(!line.is_on_line(&Point2d::new(1.0, -0.1)));
+        assert!(!line.is_on_line(&Point2d::new(0.5, -0.1)));
+        assert!(!line.is_on_line(&Point2d::new(0.0, -0.1)));
+    }
+    #[test]
+    fn on_segment() {
+        let a = Point2d::new(0.0, 0.0);
+        let b = Point2d::new(1.0, 0.0);
+        let line = Line2d::new(&a, &b);
+        assert!(!line.is_on_segment(&Point2d::new(2.0, 0.0)));
+        assert!(line.is_on_segment(&Point2d::new(1.0, 0.0)));
+        assert!(line.is_on_segment(&Point2d::new(0.5, 0.0)));
+        assert!(line.is_on_segment(&Point2d::new(0.0, 0.0)));
+        assert!(!line.is_on_segment(&Point2d::new(-1.0, 0.0)));
+        assert!(!line.is_on_segment(&Point2d::new(1.0, 0.1)));
+        assert!(!line.is_on_segment(&Point2d::new(0.5, 0.1)));
+        assert!(!line.is_on_segment(&Point2d::new(0.0, 0.1)));
+        assert!(!line.is_on_segment(&Point2d::new(1.0, -0.1)));
+        assert!(!line.is_on_segment(&Point2d::new(0.5, -0.1)));
+        assert!(!line.is_on_segment(&Point2d::new(0.0, -0.1)));
+    }
+    #[test]
+    fn intersection_point() {
+        let a = Point2d::new(0.0, 0.0);
+        let b = Point2d::new(1.0, 0.0);
+        let line = Line2d::new(&a, &b);
 
+        for x in vec![-1.0, 0.0, 0.5, 1.0, 2.0] {
+            let c = Point2d::new(x, 1.0);
+            let d = Point2d::new(x, -1.0);
+            let line2 = Line2d::new(&c, &d);
+
+            let p = line.intersection_point(&line2).unwrap();
+            assert!((x - p.x()).abs() < 1.0e-5);
+            assert!(p.y().abs() < 1.0e-5);
+            assert!(line.is_on_line(&p));
+            assert!(line2.is_on_line(&p));
+            if 0.0 <= x && x <= 1.0 {
+                assert!(line.is_on_segment(&p));
+                assert!(line2.is_on_segment(&p));
+            }
+
+            let p = line2.intersection_point(&line).unwrap();
+            assert!((x - p.x()).abs() < 1.0e-5);
+            assert!(p.y().abs() < 1.0e-5);
+            assert!(line.is_on_line(&p));
+            assert!(line2.is_on_line(&p));
+            if 0.0 <= x && x <= 1.0 {
+                assert!(line.is_on_segment(&p));
+                assert!(line2.is_on_segment(&p));
+            }
+        }
+    }
+    #[test]
+    fn intersection_parallel_case() {
+        let a = Point2d::new(0.0, 0.0);
+        let b = Point2d::new(1.0, 0.0);
+        let c = Point2d::new(0.0, 0.2);
+        let d = Point2d::new(1.0, 0.2);
+        let line = Line2d::new(&a, &b);
+        let line2 = Line2d::new(&c, &d);
+        assert_eq!(line.intersection_point(&line2), None);
+        assert_eq!(line2.intersection_point(&line), None);
+
+        let a = Point2d::new(0.0, 0.0);
+        let b = Point2d::new(0.0, 1.0);
+        let c = Point2d::new(0.0, 2.0);
+        let line = Line2d::new(&a, &b);
+        let line2 = Line2d::new(&b, &c);
+        assert_eq!(line.intersection_point(&line2), Some(b));
+        assert_eq!(line.intersection_point(&line2), Some(b));
+
+        let a = Point2d::new(0.0, 0.0);
+        let b = Point2d::new(1.0, 0.0);
+        let c = Point2d::new(-1.0, 0.0);
+        let d = Point2d::new(10.0, 0.0);
+        let line = Line2d::new(&a, &b);
+        let line2 = Line2d::new(&c, &d);
+        let p = line.intersection_point(&line2).unwrap();
+        assert!(line.is_on_segment(&p));
+        assert!(line2.is_on_segment(&p));
+
+        let p = line2.intersection_point(&line).unwrap();
+        assert!(line.is_on_segment(&p));
+        assert!(line2.is_on_segment(&p));
+    }
+    #[test]
+    fn intersect_on_segment() {
+        let a = Point2d::new(0.0, 0.0);
+        let b = Point2d::new(1.0, 0.0);
+        let line = Line2d::new(&a, &b);
+
+        for x in vec![-1.0, 0.0, 0.5, 1.0, 2.0] {
+            let c = Point2d::new(x, 1.0);
+            let d = Point2d::new(x, -1.0);
+            let line2 = Line2d::new(&c, &d);
+
+            assert_eq!(
+                line.is_intersection_point_on_both_segment(&line2),
+                0.0 <= x && x <= 1.0
+            );
+            assert_eq!(
+                line2.is_intersection_point_on_both_segment(&line),
+                0.0 <= x && x <= 1.0
+            );
+        }
+    }
     #[test]
     fn lines_iter() {
         for line in Lines2d::new(vec![
