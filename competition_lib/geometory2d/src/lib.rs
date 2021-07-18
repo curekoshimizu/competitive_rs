8c15bfdf7ee73b734715c234f6968ee5c08997b0
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-07-18 10:54:48 +0900

    Line module

--- competition_lib/geometory2d/src/lib.rs
+++ competition_lib/geometory2d/src/lib.rs
@@ -1,2 +1,3 @@
+pub mod line2d;
 pub mod point2d;
 pub mod vec2d;
---
+++ competition_lib/geometory2d/src/line2d.rs
@@ -0,0 +1,60 @@
+use super::point2d::Point2d;
+
+pub struct Lines2d {
+    pub points: Vec<Point2d>,
+}
+
+impl Lines2d {
+    pub fn new(points: Vec<Point2d>) -> Self {
+        Lines2d { points, index: 0 }
+    }
+    pub fn num_lines(&self) -> usize {
+        let len = self.points.len();
+        match len {
+            0 => 0,
+            1 => 0,
+            i => i - 1,
+        }
+    }
+
+    // TODO: understand fully why we need "impl"
+    pub fn iter(&self) -> impl Iterator<Item = (&Point2d, &Point2d)> {
+        self.points.iter().zip(self.points.iter().skip(1))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn new() {
+        let lines = Lines2d::new(vec![
+            Point2d::new(0.0, 0.0),
+            Point2d::new(1.0, 1.1),
+            Point2d::new(2.0, 2.2),
+        ]);
+        assert_eq!(lines.num_lines(), 2);
+
+        let lines = Lines2d::new(vec![Point2d::new(0.0, 0.0), Point2d::new(1.0, 1.1)]);
+        assert_eq!(lines.num_lines(), 1);
+
+        let lines = Lines2d::new(vec![Point2d::new(0.0, 0.0)]);
+        assert_eq!(lines.num_lines(), 0);
+
+        let lines = Lines2d::new(vec![]);
+        assert_eq!(lines.num_lines(), 0);
+    }
+    #[test]
+    fn iter() {
+        for (start, end) in Lines2d::new(vec![
+            Point2d::new(0.0, 0.0),
+            Point2d::new(1.0, 1.1),
+            Point2d::new(2.0, 2.2),
+        ])
+        .iter()
+        {
+            // let vec = end - start;
+            println!("{:?} {:?}", start, end);
+        }
+    }
+}
