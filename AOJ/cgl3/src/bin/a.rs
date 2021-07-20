d01f627ddd03bcafe5342f22d8625369d807dab9
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-07-20 14:20:40 +0900

    AOJ CGL3 A

---
+++ AOJ/cgl3/src/bin/a.rs
@@ -0,0 +1,19 @@
+// keywords :
+
+use geometory2d::point2d::Point2d;
+use geometory2d::polygon2d::Polygon2d;
+use io_lib::*;
+
+fn main() {
+    input! {
+        n: usize,
+        p: [f64; 2*n],
+    }
+
+    let polygon = Polygon2d::new(
+        (0..n)
+            .map(|i| Point2d::new(p[2 * i], p[2 * i + 1]))
+            .collect::<Vec<_>>(),
+    );
+    println!("{:.1}", polygon.area());
+}
