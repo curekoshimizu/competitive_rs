67baf39bdb75459f72d42ab7d9a13f87df1f233e
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-07-24 22:08:33 +0900

    AOJ ITP2_6 C

--- AOJ/itp2_6/src/bin/c.rs
+++ AOJ/itp2_6/src/bin/c.rs
@@ -1,16 +1,21 @@
 // keywords :
 
-use argio::argio;
+use io_lib::*;
+use search_lib::BinarySearch;
 
-#[argio]
-fn main(n: u64, _a: [u64; n]) {
-    todo!();
+fn main() {
+    input! {
+        n: u64,
+        a: [i64; n],
+        m: usize,
+        q: [i64; m],
+    }
+
+    for x in q {
+        if let Some(p) = a.lower_bound(&x) {
+            println!("{}", p);
+        } else {
+            println!("{}", a.len());
+        }
+    }
 }
-
-// use io_lib::*;
-//
-// fn main() {
-//     input! {
-//         n: u64,
-//     }
-// }
