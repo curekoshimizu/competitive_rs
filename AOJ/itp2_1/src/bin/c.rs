83bcd211002be1fbb8a650bf8e8bb00ac7c94022
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-07-24 19:48:41 +0900

    ITP2_1 D

--- AOJ/itp2_1/src/bin/c.rs
+++ AOJ/itp2_1/src/bin/c.rs
@@ -1,16 +1,47 @@
 // keywords :
 
-use argio::argio;
+use io_lib::*;
+use std::collections::VecDeque;
 
-#[argio]
-fn main(n: u64, _a: [u64; n]) {
-    todo!();
+fn main() {
+    let mut sc = Scanner::new(std::io::stdin().lock());
+
+    let n = sc.next::<usize>().unwrap();
+    let mut vec = VecDeque::new();
+
+    let mut pos = vec.len() as isize;
+
+    for _ in 0..n {
+        let q = sc.next::<u32>().unwrap();
+        match q {
+            0 => {
+                let x = sc.next::<i64>().unwrap();
+                if pos < vec.len() as isize {
+                    let pos = pos as usize;
+                    let mut v1 = vec.drain(pos..).collect::<VecDeque<_>>();
+                    vec.push_back(x);
+                    vec.append(&mut v1);
+                } else {
+                    vec.push_back(x);
+                }
+            }
+            1 => {
+                let d = sc.next::<isize>().unwrap();
+                pos += d;
+            }
+            2 => {
+                if pos < vec.len() as isize {
+                    vec.remove(pos as usize);
+                } else {
+                    vec.pop_back();
+                    pos = vec.len() as isize;
+                }
+            }
+            _ => panic!("!"),
+        }
+    }
+
+    for x in vec {
+        println!("{}", x);
+    }
 }
-
-// use io_lib::*;
-//
-// fn main() {
-//     input! {
-//         n: u64,
-//     }
-// }
