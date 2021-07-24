d3c89805d4f28f88f16b8664f24bb1ec6ef12f81
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-07-24 21:27:06 +0900

    AOJ ITP2_3 D

--- AOJ/itp2_3/src/bin/d.rs
+++ AOJ/itp2_3/src/bin/d.rs
@@ -1,16 +1,34 @@
 // keywords :
 
-use argio::argio;
+use io_lib::*;
 
-#[argio]
-fn main(n: u64, _a: [u64; n]) {
-    todo!();
+fn main() {
+    input! {
+        n: usize,
+        a: [i64; n],
+        m: usize,
+        b: [i64; m],
+    }
+
+    let mut i = 0;
+
+    loop {
+        if i >= a.len() && i >= b.len() {
+            println!("0");
+            break;
+        }
+
+        let x = if i < a.len() { a[i] } else { -1 };
+        let y = if i < b.len() { b[i] } else { -1 };
+
+        if x < y {
+            println!("1");
+            break;
+        } else if x > y {
+            println!("0");
+            break;
+        } else {
+        }
+        i += 1
+    }
 }
-
-// use io_lib::*;
-//
-// fn main() {
-//     input! {
-//         n: u64,
-//     }
-// }
