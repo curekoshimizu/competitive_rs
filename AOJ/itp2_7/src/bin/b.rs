0bad6f4c8e39af689763fad49fa5f0b796d720e7
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-07-24 22:17:18 +0900

    AOJ ITP2_7 B

--- AOJ/itp2_7/src/bin/b.rs
+++ AOJ/itp2_7/src/bin/b.rs
@@ -1,16 +1,30 @@
 // keywords :
 
-use argio::argio;
+use io_lib::*;
+use std::collections::HashSet;
 
-#[argio]
-fn main(n: u64, _a: [u64; n]) {
-    todo!();
+fn main() {
+    input! {
+        n: u64,
+        q: [(u64, u64); n],
+    }
+
+    let mut set = HashSet::new();
+
+    for (k, x) in q {
+        match k {
+            0 => {
+                set.insert(x);
+                println!("{}", set.len());
+            }
+            1 => {
+                let ret = if set.contains(&x) { 1 } else { 0 };
+                println!("{}", ret);
+            }
+            2 => {
+                set.remove(&x);
+            }
+            _ => panic!("!!!!"),
+        }
+    }
 }
-
-// use io_lib::*;
-//
-// fn main() {
-//     input! {
-//         n: u64,
-//     }
-// }
