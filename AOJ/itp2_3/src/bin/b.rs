2342a670f8d195ccdb68d6d3888014e35ae4810e
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-07-24 21:11:25 +0900

    AOJ ITP2_3 B

--- AOJ/itp2_3/src/bin/b.rs
+++ AOJ/itp2_3/src/bin/b.rs
@@ -1,16 +1,22 @@
 // keywords :
 
-use argio::argio;
+use io_lib::*;
 
-#[argio]
-fn main(n: u64, _a: [u64; n]) {
-    todo!();
+fn main() {
+    input! {
+        n: usize,
+        a: [i64; n],
+        q: usize,
+        vec: [(usize, usize, usize); q],
+    }
+
+    for (q, x, y) in vec {
+        if q == 0 {
+            let min = a[x..y].iter().min().unwrap();
+            println!("{}", min);
+        } else {
+            let max = a[x..y].iter().max().unwrap();
+            println!("{}", max);
+        }
+    }
 }
-
-// use io_lib::*;
-//
-// fn main() {
-//     input! {
-//         n: u64,
-//     }
-// }
