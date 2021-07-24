5357a09c85bd60164c0e938538a1c302519ddbdb
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-07-24 23:32:48 +0900

    AOJ ITP2_10 D

--- AOJ/itp2_10/src/bin/d.rs
+++ AOJ/itp2_10/src/bin/d.rs
@@ -1,16 +1,78 @@
 // keywords :
 
-use argio::argio;
+use io_lib::*;
 
-#[argio]
-fn main(n: u64, _a: [u64; n]) {
-    todo!();
+fn main() {
+    let mut sc = Scanner::new(std::io::stdin().lock());
+
+    let n = sc.next::<usize>().unwrap();
+    let mut masks = vec![0u64; n];
+    for index in 0..n {
+        let m = sc.next::<usize>().unwrap();
+        for _ in 0..m {
+            let i = sc.next::<usize>().unwrap();
+            masks[index] |= 1 << i;
+        }
+    }
+
+    let mut x: u64 = 0;
+
+    let n = sc.next::<usize>().unwrap();
+    for _ in 0..n {
+        let q = sc.next::<usize>().unwrap();
+        match q {
+            0 => {
+                let i = sc.next::<usize>().unwrap();
+                let ret = if (x & (1 << i)) > 0 { 1 } else { 0 };
+                println!("{}", ret);
+            }
+            1 => {
+                let i = sc.next::<usize>().unwrap();
+                let m = masks[i];
+                x |= m;
+            }
+            2 => {
+                let i = sc.next::<usize>().unwrap();
+                let m = masks[i];
+                x &= !m;
+            }
+            3 => {
+                let i = sc.next::<usize>().unwrap();
+                let m = masks[i];
+                let y = !x & m;
+                x = (x & !m) | y;
+            }
+            4 => {
+                let i = sc.next::<usize>().unwrap();
+                let m = masks[i];
+                let ret = if x & m == m { 1 } else { 0 };
+                println!("{}", ret);
+            }
+            5 => {
+                let i = sc.next::<usize>().unwrap();
+                let m = masks[i];
+                let ret = if x & m != 0 { 1 } else { 0 };
+                println!("{}", ret);
+            }
+            6 => {
+                let i = sc.next::<usize>().unwrap();
+                let m = masks[i];
+                let ret = if x & m == 0 { 1 } else { 0 };
+                println!("{}", ret);
+            }
+            7 => {
+                let i = sc.next::<usize>().unwrap();
+                let m = masks[i];
+                println!("{}", (x & m).count_ones());
+            }
+            8 => {
+                let i = sc.next::<usize>().unwrap();
+                let m = masks[i];
+                println!("{}", x & m);
+            }
+            _ => {
+                panic!("!!!!!!!!!!");
+            }
+        }
+    }
 }
-
-// use io_lib::*;
-//
-// fn main() {
-//     input! {
-//         n: u64,
-//     }
-// }
