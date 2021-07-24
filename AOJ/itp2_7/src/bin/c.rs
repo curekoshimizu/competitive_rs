5d1714e75fe5ea2212390410488b4fb47188ac7d
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-07-24 22:24:11 +0900

    AOJ ITP2_7 C

--- AOJ/itp2_7/src/bin/c.rs
+++ AOJ/itp2_7/src/bin/c.rs
@@ -1,16 +1,41 @@
 // keywords :
 
-use argio::argio;
+use io_lib::*;
+use std::collections::BTreeSet;
+use std::ops::Bound::Included;
 
-#[argio]
-fn main(n: u64, _a: [u64; n]) {
-    todo!();
+fn main() {
+    let mut sc = Scanner::new(std::io::stdin().lock());
+
+    let n = sc.next::<usize>().unwrap();
+
+    let mut set = BTreeSet::new();
+
+    for _ in 0..n {
+        let k = sc.next::<u64>().unwrap();
+        let x = sc.next::<u64>().unwrap();
+
+        match k {
+            0 => {
+                set.insert(x);
+                println!("{}", set.len());
+            }
+            1 => {
+                let ret = if set.contains(&x) { 1 } else { 0 };
+                println!("{}", ret);
+            }
+            2 => {
+                set.remove(&x);
+            }
+            3 => {
+                let l = x;
+                let r = sc.next::<u64>().unwrap();
+
+                for &e in set.range((Included(&l), Included(&r))) {
+                    println!("{}", e);
+                }
+            }
+            _ => panic!("!!!!"),
+        }
+    }
 }
-
-// use io_lib::*;
-//
-// fn main() {
-//     input! {
-//         n: u64,
-//     }
-// }
