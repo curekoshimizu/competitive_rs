af1cf6ea4ba62b354130b9f93d1a943c462af9e3
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-07-24 22:35:43 +0900

    AOJ ITP2_8 B

--- AOJ/itp2_8/src/bin/b.rs
+++ AOJ/itp2_8/src/bin/b.rs
@@ -1,16 +1,32 @@
 // keywords :
 
-use argio::argio;
+use io_lib::*;
+use std::collections::BTreeMap;
 
-#[argio]
-fn main(n: u64, _a: [u64; n]) {
-    todo!();
+fn main() {
+    let mut sc = Scanner::new(std::io::stdin().lock());
+
+    let n = sc.next::<usize>().unwrap();
+
+    let mut map = BTreeMap::new();
+
+    for _ in 0..n {
+        let k = sc.next::<u64>().unwrap();
+        let x = sc.next::<String>().unwrap();
+
+        match k {
+            0 => {
+                let v = sc.next::<u64>().unwrap();
+                map.insert(x, v);
+            }
+            1 => {
+                let ret = map.get(&x).unwrap_or(&0);
+                println!("{}", ret);
+            }
+            2 => {
+                map.remove(&x);
+            }
+            _ => panic!("!!!!"),
+        }
+    }
 }
-
-// use io_lib::*;
-//
-// fn main() {
-//     input! {
-//         n: u64,
-//     }
-// }
