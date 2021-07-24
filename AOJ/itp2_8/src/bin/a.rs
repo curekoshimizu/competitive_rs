ec943fce57219430595d80ff4b2848f9545f6265
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-07-24 22:33:56 +0900

    AOJ ITP2_8 A

---
+++ AOJ/itp2_8/src/bin/a.rs
@@ -0,0 +1,29 @@
+// keywords :
+
+use io_lib::*;
+use std::collections::BTreeMap;
+
+fn main() {
+    let mut sc = Scanner::new(std::io::stdin().lock());
+
+    let n = sc.next::<usize>().unwrap();
+
+    let mut map = BTreeMap::new();
+
+    for _ in 0..n {
+        let k = sc.next::<u64>().unwrap();
+        let x = sc.next::<String>().unwrap();
+
+        match k {
+            0 => {
+                let v = sc.next::<u64>().unwrap();
+                map.insert(x, v);
+            }
+            1 => {
+                let ret = map.get(&x).unwrap();
+                println!("{}", ret);
+            }
+            _ => panic!("!!!!"),
+        }
+    }
+}
