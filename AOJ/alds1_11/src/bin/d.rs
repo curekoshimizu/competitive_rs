017538a72071f8e410819fd59bccc76f41bc35d9
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-07-25 11:17:39 +0900

    AOJ ALDS1_11 D

--- AOJ/alds1_11/src/bin/d.rs
+++ AOJ/alds1_11/src/bin/d.rs
@@ -1,16 +1,23 @@
 // keywords :
 
-use argio::argio;
+use collection_lib::union_find::UnionFind;
+use io_lib::*;
 
-#[argio]
-fn main(n: u64, _a: [u64; n]) {
-    todo!();
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        r: [(usize, usize); m],
+        k: usize,
+        q: [(usize, usize); k],
+    }
+
+    let mut uf = UnionFind::new(n);
+    for (x, y) in r {
+        uf.unite(x, y);
+    }
+    for (x, y) in q {
+        let ret = if uf.is_same(x, y) { "yes" } else { "no" };
+        println!("{}", ret);
+    }
 }
-
-// use io_lib::*;
-//
-// fn main() {
-//     input! {
-//         n: u64,
-//     }
-// }
