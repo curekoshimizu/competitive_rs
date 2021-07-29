5cf900a9f2e639bc79f086892d167df80661f161
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-07-29 23:15:23 +0900

    AOJ DPL_1 B

--- AOJ/dpl_1/src/bin/b.rs
+++ AOJ/dpl_1/src/bin/b.rs
@@ -1,16 +1,29 @@
 // keywords :
 
-use argio::argio;
+use io_lib::*;
 
-#[argio]
-fn main(n: u64, _a: [u64; n]) {
-    todo!();
+fn main() {
+    input! {
+        n: usize,
+        ww: usize,
+        q: [(u64, usize); n],
+    }
+
+    let mut dp_prev = vec![0; ww + 1];
+
+    for (v, w) in q {
+        let mut dp_new = vec![0; ww + 1];
+        for j in 0..=ww {
+            if j >= w {
+                let index = j - w;
+                let x = dp_prev[index] + v;
+                let y = dp_prev[j];
+                dp_new[j] = u64::max(x, y);
+            } else {
+                dp_new[j] = dp_prev[j];
+            }
+        }
+        dp_prev = dp_new;
+    }
+    println!("{}", dp_prev[ww]);
 }
-
-// use io_lib::*;
-//
-// fn main() {
-//     input! {
-//         n: u64,
-//     }
-// }
