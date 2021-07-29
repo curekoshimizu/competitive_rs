3c73a63de3af9f93db5d4e218839f2763c454c93
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-07-29 22:57:46 +0900

    AOJ DPL_1 A

--- AOJ/dpl_1/src/bin/a.rs
+++ AOJ/dpl_1/src/bin/a.rs
@@ -16,9 +16,14 @@ fn main() {
 
     let mut dp: Vec<u64> = (0..=n).collect();
 
-    for &x in c[1..].iter() {
-        dp[x as usize] = 1;
-        dp[x as usize + 1] = 1;
-        for _i in 0..=n {}
+    for &c in c[1..].iter() {
+        for i in 0..=n {
+            if i >= c {
+                let index = (i - c) as usize;
+                let i = i as usize;
+                dp[i] = u64::min(dp[index].saturating_add(1), dp[i]);
+            }
+        }
     }
+    println!("{}", dp[n as usize]);
 }
