223ec05fd9e11296203dc794471799422b9df547
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-07-30 13:46:35 +0900

    AOJ DPL_1 D

--- AOJ/dpl_1/src/bin/f.rs
+++ AOJ/dpl_1/src/bin/f.rs
@@ -1,16 +1,32 @@
 // keywords :
 
-use argio::argio;
+use io_lib::*;
 
-#[argio]
-fn main(n: u64, _a: [u64; n]) {
-    todo!();
+fn main() {
+    input! {
+        n: usize,
+        max_w: u64,
+        a: [(usize, u64); n], // (v, w)
+    }
+
+    let max_v = 10000usize;
+
+    let mut dp = vec![u64::max_value(); max_v + 1];
+    dp[0] = 0;
+
+    for (v, w) in a {
+        for i in (0..=max_v).rev() {
+            if i >= v {
+                let index = i - v;
+                dp[i] = u64::min(dp[index].saturating_add(w), dp[i]);
+            }
+        }
+    }
+
+    for i in (0..=max_v).rev() {
+        if dp[i] <= max_w {
+            println!("{}", i);
+            break;
+        }
+    }
 }
-
-// use io_lib::*;
-//
-// fn main() {
-//     input! {
-//         n: u64,
-//     }
-// }
